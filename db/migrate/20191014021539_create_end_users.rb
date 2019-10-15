28f2f9d22712f587c9e29858a142de61701527ba
kenmakonao <wearekenchan@gmail.com>
2019-10-15 10:23:18 +0000

    かラムの変更

--- db/migrate/20191014021539_create_end_users.rb
+++ db/migrate/20191014021539_create_end_users.rb
@@ -1,18 +1,18 @@
 class CreateEndUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :end_users do |t|
-      t.string :kanzi_last_name
-      t.string :string
-      t.string :kanzi_first_name
-      t.string :string
-      t.string :kana_last_name
-      t.string :string
-      t.string :kana_first_name
-      t.string :string
-      t.string :email
-      t.string :string
-      t.string :possword
-      t.string :string
+      t.string :kanzi_last_name, null: false
+
+      t.string :kanzi_first_name, null: false
+
+      t.string :kana_last_name, null: false
+
+      t.string :kana_first_name, null: false
+
+      t.string :possword, null: false
+
+      t.datetime :daleted_at
+
 
       t.timestamps
     end
--- db/migrate/20191014022506_create_product_in_carts.rb
+++ db/migrate/20191014022506_create_product_in_carts.rb
@@ -1,12 +1,12 @@
 class CreateProductInCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :product_in_carts do |t|
-      t.string :end_user_id
-      t.string :integer
-      t.string :product_id
-      t.string :integer
-      t.string :sheet
-      t.string :integr
+      t.integer :end_user_id, null: false
+
+      t.integer :product_id, null: false
+ 
+      t.integer :sheet, null: false
+
 
       t.timestamps
     end
