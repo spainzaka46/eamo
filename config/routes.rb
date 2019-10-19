0ab957daebb3612714523ba0c97dbb7e46bd629e
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-10-19 05:34:56 +0000

    [Modify] ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,10 @@ get 'end_user/finish' => 'end_user/users#finish', as: 'end_user_finish'
 namespace :admin do
 	resources :products,only: [:index, :show, :edit, :update, :new, :create]
 	resources :users,only: [:index, :show, :edit, :update, :destroy] do
-		get '/orderhistories' => 'users#orderhistories',as: 'orderhistories'
+		get '/orderhistories' => 'orderhistories#index',as: 'orderhistories'
 	end
 	resources :arrivals,only: [:index, :new, :create]
 	resources :orders,only: [:index, :show,]
-	resources :orderhistories,only: [:index,]
 	resources :tops,only: [:index,]
 end
 # resources :admin_sessions,only: [:destroy, :new, :create]
