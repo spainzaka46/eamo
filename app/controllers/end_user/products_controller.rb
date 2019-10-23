8b25b3b066cf95b158ee47acbbe498a64adc82f7
takeshi1201 <kodama19961201@gmail.com>
2019-10-23 03:23:25 +0000

    コミット

--- app/controllers/end_user/products_controller.rb
+++ app/controllers/end_user/products_controller.rb
@@ -10,13 +10,13 @@ class EndUser::ProductsController < ApplicationController
 
   def show
   	@product = Product.find(params[:id])
-    @artist = @product.artist	
-    @genre = @product.genre	
-    @label = @product.label	
+    @artist = @product.artist
+    @genre = @product.genre
+    @label = @product.label
     order = OrderDetail.where(product_id:@product.id).group(:product_id).sum(:sheet)	
-    ordersum = order[@product.id]	
+    ordersum = order[@product.id]
     arrival = ArrivalOfGood.where(product_id:@product.id).group(:product_id).sum(:sheet)	
-    arrivalsum = arrival[@product.id]	
+    arrivalsum = arrival[@product.id]
     @stock = arrivalsum - ordersum
 
   end
