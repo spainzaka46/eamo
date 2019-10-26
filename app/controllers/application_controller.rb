04369935273e33d3762923477094be1191d91fe1
kenmakonao <wearekenchan@gmail.com>
2019-10-26 04:31:09 +0000

    エラー解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
  before_action :set_genres
- def after_sign_in_path_for(resource)
-  	mypage_path(resource)
- end
+
 protected
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:kanzi_last_name,:kanzi_first_name,:kana_last_name,:kana_first_name,:email,:password])
