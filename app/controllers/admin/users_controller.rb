e30c9a26a9a978ec07bada793b3629f751780cc0
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-10-28 07:44:46 +0000

    Fix ボタン修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,8 +6,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-  	@end_user=EndUser.find(params[:id])
-
+  	@end_user=EndUser.with_deleted.find(params[:id])
   end
 
   def edit
@@ -16,7 +15,7 @@ class Admin::UsersController < ApplicationController
   def destroy
     @end_user = EndUser.find(params[:id])
     @end_user.destroy
-    redirect_to dmin_users_path
+    redirect_to admin_users_path
   end
 
   def updatedef update
