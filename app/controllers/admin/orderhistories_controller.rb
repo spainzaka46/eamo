8a6a25e2cb47badf73a6a1cfd2e4f0a8afd14fd9
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-10-29 07:59:21 +0000

    論理削除機能完成

--- app/controllers/admin/orderhistories_controller.rb
+++ app/controllers/admin/orderhistories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrderhistoriesController < ApplicationController
 	before_action :authenticate_admin!
   def index
-    end_user = EndUser.find(params[:user_id])
-    @orders = end_user.orders.page(params[:page]).reverse_order
+    end_user = EndUser.with_deleted.find(params[:user_id])
+    @orders = end_user.orders.with_deleted.page(params[:page]).reverse_order
   end
 end
