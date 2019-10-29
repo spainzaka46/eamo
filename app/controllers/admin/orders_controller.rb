c8e85c74fa545c297cdb0f1eba98b4a5a8c87e95
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-10-29 04:34:39 +0000

    admin側の受注一覧最新の時間順に変更

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
 	before_action :authenticate_admin!
 
   def index
-    @orders = Order.page(params[:page]).per(10)
+    @orders = Order.page(params[:page]).order(created_at: :desc).per(10)
   end
 
   def show
